fe8696fde3ad90444d3db6bba41bfcc3afd9b3ce
serge <mail@r-serge.info>
2018-04-05 15:41:07 +1000

    * Adding parse_inventory tool for test TextFSM templates

---
+++ utils/parse_inventory.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import clitable
+from pprint import pprint
+
+input_file = sys.stdin.read()
+
+cli_table = clitable.CliTable('index', '../cli_templates')
+
+attributes = {'Command': sys.argv[1]}
+print(attributes)
+
+cli_table.ParseCmd(input_file, attributes)
+keys = cli_table.header.values
+
+result = [dict(zip(keys,row)) for row in cli_table]
+
+pprint(result)
